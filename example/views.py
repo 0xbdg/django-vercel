5a840bf4f06f9d462646d4f7ef4ebb241fdc705b
?0 <benjamindguntara@protonmail.com>
2024-10-09 11:58:13 +0700

    dwq

--- example/views.py
+++ example/views.py
@@ -4,4 +4,4 @@ from datetime import datetime
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "pages/index.html", context={})
+    return render(request, "pages/index.html")
